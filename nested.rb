f9ab18469ad9b352a2cd50bb68b12c1291972666
Mustafa Muse <umpp101@icloud.com>
2019-09-07 20:59:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-programmer_hash [:yukihiro_matsumoto] => {
+programmer_hash[:yukihiro_matsumoto] => {
           :known_for => "Ruby",
           :languages => ["LISP", "C"]
       }
